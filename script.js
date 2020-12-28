a5ce2099c7c5cdb0468f5beb72401b914b84d6ab
Anastasia Diachenko <an.shardakova@gmail.com>
2020-12-28 23:41:10 +0200

    homework_28.12

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 10000;
+let income = 'freelance';
+let addExpenses = ['internet', 'transport', 'utilities'];
+let deposit = true;
+let mission = 5000;
+let period = 12;
+let budgetDay = money / 30;
 
-console.log('Задание готово!');
-alert('Задание готово!');
+console.log(typeof (money));
+console.log(typeof (income));
+console.log(typeof (deposit));
+
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' гривен');
+
+//console.log(addExpenses.toLowerCase());
+
+console.log(budgetDay);
